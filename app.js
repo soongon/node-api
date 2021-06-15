d76286ca55d55d434fa88966522e44784dd3ac9a
soongon <soongon@hucloud.co.kr>
2021-06-15 20:45:56 +0900

    initial commit

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('hello world...');
+app.get('/', (req, res) => {
+    res.send('홈입니다.');
 });
 
-app.get('/hello', function (req, res) {
-    res.send('안녕 친구들.....hi hi hi gu hi sdfdklsjfdklsfj');
+app.get('/users', (req, res) => {
+    res.send('모든 글 보기');
 });
 
-app.listen(3000);
-console.log('server ready on port 3000');
+app.get('/users/1', (req, res) => {
+    res.send('1번 글 상세보기');
+});
+
+app.listen(3000, () => {
+    console.log('server ready on port 3000...');
+});
