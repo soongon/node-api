1bb13df28f11b16c4d3b5e6aa84c7b348c98ba98
soongon <soongon@hucloud.co.kr>
2021-06-15 20:32:37 +0900

    initial commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/hello', function (req, res) {
-    res.send('hello again..');
+    res.send('안녕 친구들.....hi hi hi gu hi sdfdklsjfdklsfj');
 });
 
 app.listen(3000);
