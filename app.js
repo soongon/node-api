490d026d3aacab133ffbb889f231d532dfdc59c3
soongon <soongon@hucloud.co.kr>
2021-06-16 19:42:23 +0900

    initial commit

--- app.js
+++ app.js
@@ -6,15 +6,56 @@ const app = express();
 // middlewares
 app.use(morgan('tiny'));
 
-app.get('/', (req, res) => {
-    res.json('홈입니다.');
+app.get('/posts', (req, res) => {
+    const dataFromDB = [
+        {
+            userId: 1,
+            id: 1,
+            title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        },
+        {
+            userId: 1,
+            id: 2,
+            title: "qui est esse",
+            body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+        },
+        {
+            userId: 1,
+            id: 3,
+            title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+            body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+        }
+    ];
+    res.json(dataFromDB);
 });
-app.get('/users', (req, res) => {
-    res.redirect('/users/1');
+app.get('/posts/:id', (req, res) => {
+    if (req.params.id === '1') {
+        res.json({
+            userId: 1,
+            id: 1,
+            title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        });
+    } else if (req.params.id === '2') {
+        res.json({
+            userId: 1,
+            id: 2,
+            title: "qui est esse",
+            body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+        });
+    } else if (req.params.id === '3') {
+        res.json({
+            userId: 1,
+            id: 3,
+            title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+            body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+        });
+    } else {
+        res.json('error...');
+    }
 });
-app.get('/users/1', (req, res) => {
-    res.send('1번 글 상세보기');
-});
+
 app.get('*', (req, res) => {
     res.send('잘못된 경로입니다.');
 })
